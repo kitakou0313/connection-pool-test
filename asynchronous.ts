892c5094f8a786fc1ee4ccd0e43195acefb8f96b
kitakou0313 <kitakou0313@gmail.com>
2025-01-30 07:53:13 +0900

    memo: add memo about async await

--- asynchronous.ts
+++ asynchronous.ts
@@ -22,3 +22,65 @@ promise.then((result) => {
 // await keyword
 // Promiseがreject, resolveされるまで処理を停止する．
 // async functionの中でのみ使用可能
+// ESではTop level awaitというのもある
+
+async function fetchData() {
+    try {
+        let response = await fetch("localhost:300/test");
+        let data = await response.json()
+
+        return data
+    } catch (error) {
+        console.error("Error:", error)
+    }
+}
+
+const res = await fetchData()
+
+// パターン
+// Promiseを返す + asyncあり
+async function pattern1(){
+    let promise = new Promise<number>((resolve, reject) => {
+        // 何らかの非同期処理
+
+        // 成功時
+        resolve(1)
+
+        // 失敗時
+        reject("Task is failed")
+    })
+    
+    return promise
+}
+
+const resOfPattern1 = pattern1()
+
+// Promiseを返す + asyncなし 
+function pattern2(){
+    let promise = new Promise<number>((resolve, reject) => {
+        // 何らかの非同期処理
+
+        // 成功時
+        resolve(1)
+
+        // 失敗時
+        reject("Task is failed")
+    })
+    
+    return promise
+}
+const resOfPattern2 = pattern2()
+
+// Promiseを返さない + asyncあり
+// Promiseを返す関数扱いになる
+async function pattern3(){
+    return 1
+}
+
+const resOfPattern3 = pattern3()
+
+// Promiseを返さない + asyncなし
+// これは通常の関数
+function pattern4(){
+    return 1
+}
